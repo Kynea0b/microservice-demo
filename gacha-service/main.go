00dcd472685121f1784619b8d412621a1238869f
JY8752 <j.y.8752@gmail.com>
2023-06-21 04:10:50 +0900

    implement gacha service

--- gacha-service/main.go
+++ gacha-service/main.go
@@ -16,28 +16,61 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+const (
+	port = ":3306"
+	db   = "Gacha"
+)
+
+func getDatasourceName() (string, error) {
+	user := os.Getenv("DB_USER")
+	if user == "" {
+		return "", fmt.Errorf("DB_USER is not set")
+	}
+
+	pass := os.Getenv("DB_PASSWORD")
+	if pass == "" {
+		return "", fmt.Errorf("DB_PASSWORD is not set")
+	}
+
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		return "", fmt.Errorf("DB_HOST is not set")
+	}
+
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		user,
+		pass,
+		host,
+		port,
+		db,
+	), nil
+}
+
+func connectDb(dataSourceName string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	log.Println("Start Gacha Service")
 
-	// Init DB
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
+	dsn, err := getDatasourceName()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Open a sql.DB connection pool
-	db, err := sql.Open("mysql", dsn)
+	db, err := connectDb(dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
-	// if err = db.Ping(); err != nil {
-	// 	log.Fatal(err)
-	// }
 
 	// grpc server の起動
 	listener, err := net.Listen("tcp", ":8080")
@@ -47,9 +80,16 @@ func main() {
 
 	server := grpc.NewServer()
 
-	gachapb.RegisterGachaServiceServer(server, NewGachaServiceServer())
-
 	reflection.Register(server)
+
+	// ガチャの登録
+	items := []Item{
+		{Id: 1, Name: "item1", Rarity: gachapb.Rarity_RARITY_N, Weight: 40},
+		{Id: 2, Name: "item2", Rarity: gachapb.Rarity_RARITY_R, Weight: 30},
+		{Id: 3, Name: "item3", Rarity: gachapb.Rarity_RARITY_SR, Weight: 20},
+		{Id: 4, Name: "item4", Rarity: gachapb.Rarity_RARITY_SSR, Weight: 10},
+	}
+	gachapb.RegisterGachaServiceServer(server, NewGachaServiceServer(db, items))
 
 	go func() {
 		if err := server.Serve(listener); err != nil {
--- gacha-service/service.go
+++ gacha-service/service.go
@@ -2,21 +2,99 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	gachapb "gacha-service/pkg/grpc/proto"
+	"math/rand"
+	"time"
 )
+
+type Item struct {
+	Id     int64
+	Name   string
+	Rarity gachapb.Rarity
+	Weight int
+}
 
 type gachaServiceServer struct {
 	gachapb.UnimplementedGachaServiceServer
+	db    *sql.DB
+	items []Item
 }
 
-func NewGachaServiceServer() gachapb.GachaServiceServer {
-	return &gachaServiceServer{}
+func NewGachaServiceServer(db *sql.DB, items []Item) gachapb.GachaServiceServer {
+	return &gachaServiceServer{
+		db:    db,
+		items: items,
+	}
 }
 
 func (g *gachaServiceServer) Draw(ctx context.Context, req *gachapb.DrawRequest) (*gachapb.DrawResponse, error) {
+	// itemsからitemを重み付抽選する
+	weights := make([]int, len(g.items))
+	for i, item := range g.items {
+		weights[i] = item.Weight
+	}
+
+	seed := time.Now().UnixNano()
+	i := linearSearchLottery(weights, seed)
+	item := g.items[i]
+
+	// DBに保存する
+	if err := save(ctx, g.db, req.UserId, item); err != nil {
+		return nil, err
+	}
+
 	return &gachapb.DrawResponse{
-		ItemId:   1,
-		ItemName: "item1",
-		Rarity:   gachapb.Rarity_RARITY_N,
+		ItemId:   item.Id,
+		ItemName: item.Name,
+		Rarity:   item.Rarity,
 	}, nil
+}
+
+func save(ctx context.Context, db *sql.DB, userId int64, item Item) error {
+	_, err := db.ExecContext(
+		ctx,
+		"INSERT INTO Gacha (user_id, item_id, item_name, rarity, created_at) VALUES (?, ?, ?, ?, ?)",
+		userId,
+		item.Id,
+		item.Name,
+		item.Rarity.String(),
+		time.Now().Format("2006-01-02 15:04:05"),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+/*
+線形探索で重み付抽選する
+@return 当選した要素のインデックス
+*/
+func linearSearchLottery(weights []int, seed int64) int {
+	//  重みの総和を取得する
+	var total int
+	for _, weight := range weights {
+		total += weight
+	}
+
+	// 乱数取得
+	if seed == 0 {
+		seed = time.Now().UnixNano()
+	}
+	rand.Seed(seed)
+	rnd := rand.Intn(total)
+
+	var currentWeight int
+	for i, w := range weights {
+		// 現在要素までの重みの総和
+		currentWeight += w
+
+		if rnd < currentWeight {
+			return i
+		}
+	}
+
+	// たぶんありえない
+	return len(weights) - 1
 }
